a3b7fdd40f04414dc855c61da6f259f9f7fdb1d1
EvgeN19840 <107992155+EvgeN19840@users.noreply.github.com>
2023-03-08 16:29:19 +0400

    added

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter basename="EvgeN19840.github.io/MOVI">
+  <BrowserRouter basename="/EvgeN19840" >
     <React.StrictMode>
       <Provider store={store}>
         <App />
