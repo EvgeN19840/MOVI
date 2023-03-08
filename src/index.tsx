a45acde07d9ad8bf25e449ba4ff37c40f11d9f03
EvgeN19840 <107992155+EvgeN19840@users.noreply.github.com>
2023-03-08 14:35:35 +0400

    1

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <React.StrictMode>
       <Provider store={store}>
         <App />
